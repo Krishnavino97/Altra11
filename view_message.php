390a983b83ff0f98aeed60718b7eb2adbf44dd67
Vinoji Sureshkumar <70386403+Krishnavino97@users.noreply.github.com>
2021-03-27 15:45:40 +0530

    Update view_message.php

--- view_message.php
+++ view_message.php
@@ -1,3 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="dashboard.css">
+<link rel="stylesheet" type="text/css" href="view_user.css">
+<link rel="stylesheet" type="text/css" href="popup.css">
+<link rel="stylesheet" href="icons/font-awesome/css/font-awesome.min.css">
+</head>
+
+<body style ="background-color:#a7bfc9">
+
+
+
+<div class = "grid">
+		<form align = "right" method = "POST" action = "message_pdf.php">
+		<button type = "submit" class="pdf" name = "pdf"><img class = "icon" src = "images/pdf.png" /> PDF </button> </a>
+		</form>
+	</div>
+
+<br>
+
 <?php
 $servername = "localhost";
 $username = "root";
@@ -11,7 +32,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT firstname, lastname, email, message FROM messages";
+$sql = "SELECT no,fname, lname, email, subject FROM message";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
@@ -20,17 +41,14 @@ if ($result->num_rows > 0) {
 			<th>Full Name</th>
 			<th>Email</th>
 			<th>Message</th>
-			<th>Reply</th>
-			<th>Remove</th>
 			</tr>";
     // output data of each row
     while($row = $result->fetch_assoc()) {
         echo "<tr>
-				<td>" . $row["firstname"] . $row["lastname"]. "</td>
+				<td>" . $row["fname"] . $row["lname"]. "</td>
 				<td>" . $row["email"]. "</td>
-				<td>" . $row["message"]. "</td>
-				<td><button> Reply </button></td>
-				<td><button> Remove </button></td>
+				<td>" . $row["subject"]. "</td>
+			
 				</tr>";
     }
     echo "</table>";
@@ -39,4 +57,24 @@ if ($result->num_rows > 0) {
 }
 
 $conn->close();
+
+echo 
+				"<script type = text/javascript>
+				function openRly() {
+					document.getElementById(rlyForm).style.d
+					display = block;
+				}
+
+				function closeRly() {
+					document.getElementById(rlyForm).style.display = none;
+				}
+				</script>";
+				
+
 ?>
+<br>
+</div>
+
+
+</body>
+</html>
